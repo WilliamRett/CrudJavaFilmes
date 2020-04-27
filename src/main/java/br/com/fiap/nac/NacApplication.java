a25a66fb8bb80e895d4ceb55c8cead0522a826ea
william.rett <william.rett>
2020-04-27 19:30:05 -0300

    subindo projeto

---
+++ src/main/java/br/com/fiap/nac/NacApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.nac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacApplication.class, args);
+	}
+
+}
